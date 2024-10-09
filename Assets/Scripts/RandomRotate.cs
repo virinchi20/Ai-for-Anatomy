83999ca139d3865a7e16b968af5b6b8eea40fe73
samselesky <samselesky2003@gmail.com>
2024-10-09 15:28:04 -0400

    Add functionality for detecting if heart is in natural placement

--- Assets/Scripts/RandomRotate.cs
+++ Assets/Scripts/RandomRotate.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class RandomRotate : MonoBehaviour
 {
 
-    public GameObject obj;
+    public GameObject obj, continueButton, buttonGroup;
+    public TextMeshProUGUI correctText;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +27,24 @@ public class RandomRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(obj.transform.rotation.eulerAngles);
+        // If heart is close to natural position (+= 10 degrees), activate correct button
+        // Else deactivate correct button
+        if ((obj.transform.rotation.eulerAngles.x % 360.0 <= 10.0 || obj.transform.rotation.eulerAngles.x % 360.0 >= 350.0)
+            && (obj.transform.rotation.eulerAngles.y % 180.0 <= 10.0 || obj.transform.rotation.eulerAngles.y % 180.0 >= 170.0)
+            && (obj.transform.rotation.eulerAngles.z % 360.0 <= 10.0 || obj.transform.rotation.eulerAngles.z % 360.0 >= 350.0))
+        {
+            buttonGroup.SetActive(true);
+            continueButton.SetActive(true);
+            correctText.text = "<size=8>Correct Placement</size>";
+            correctText.color = Color.green;
+        }
+        else
+        {
+            buttonGroup.SetActive(false);
+            continueButton.SetActive(false);
+            correctText.text = "<size=8>Incorrect Placement</size>";
+            correctText.color = Color.red;
+        }
     }
 }
